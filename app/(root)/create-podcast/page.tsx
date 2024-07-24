0c528d325dcd9fe62e23e4788e00b431a752fa1a
Nuttyqueen <pair25525@gmail.com>
2024-07-24 11:15:35 +0700

    podcast layout

---
+++ app/(root)/create-podcast/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CreatePodcast = () => {
+  return (
+    <div>
+      <section className="flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">Create Podcast</h1>
+      </section>
+    </div>
+  );
+};
+
+export default CreatePodcast;
---
+++ app/(root)/discover/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Discover = () => {
+  return (
+    <div>
+      <section className="flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">Discover</h1>
+      </section>
+    </div>
+  );
+};
+
+export default Discover;
